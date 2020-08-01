9eddff1f74aa6b92c9aa885eaf0488c7aedc2f12
cglusky <43158+cglusky@users.noreply.github.com>
2020-08-01 13:15:49 -0500

    created arhives folder

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,9 +11,13 @@ then
     echo "PostgreSQL started"
 fi
 
-# Uncomment below to flush db e.g. after running tests 
+# Uncomment below to flush db e.g. after running tests
+# Just make sure you really mean it 
 # python manage.py flush --no-input
+
+# We have base custom user model so need to makemigrations out of box
 python manage.py makemigrations core
+
 python manage.py migrate
 python manage.py collectstatic --noinput
 
