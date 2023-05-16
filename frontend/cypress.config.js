2f77ab1ea9175b20c353581ae4a5761703c9921f
Dan Bayford <danbeeweb@gmail.com>
2023-05-16 16:36:17 +0100

    edit yml

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  screenshotOnRunFailure: false,
+  video: false
 });
