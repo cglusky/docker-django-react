4c2043cb41959b4f014f0a2221ab8d0e04ddaec7
Dan Bayford <danbeeweb@gmail.com>
2023-05-26 12:40:09 +0100

    remove db backup

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // baseUrl: 'http://react:3000',
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://nginx',
+    // baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
